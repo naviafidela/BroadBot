4c43f57174345769ab0aaa93d6b5a3a32073a8cf
naviafidela <naviafidela@gmail.com>
2025-09-23 00:31:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get("API_ID", "20786693"))
 API_HASH = os.environ.get("API_HASH", "6eebbb7d9f9825a2d200c034bfbb7102")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002548704888"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "8181491671").split())
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://naviafidela:xILLUSION@10@broadcastbot.t920fzc.mongodb.net/?retryWrites=true&w=majority&appName=BroadcastBot")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://naviafidela:xILLUSION@10@broadcastbot.t920fzc.mongodb.net/")
 DB_NAME = os.environ.get("DB_NAME", "naviafidela")
 
 # broadcast settings
